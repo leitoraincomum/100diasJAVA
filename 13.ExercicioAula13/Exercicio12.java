57f221ac830e1e2c138e6b5e1ef24aa844cae61d
leitoraincomum <fcristinadesouza@gmail.com>
2021-07-22 22:39:43 -0300

    ate exercicio 12

---
+++ 13.ExercicioAula13/Exercicio12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Exercicio12 {
+    public static void main(String[] args) {
+        /*12. Tendo como dados de entrada a altura de uma pessoa, 
+        construa um algoritmo que calcule seu peso ideal, 
+        usando a seguinte fórmula: (72.7*altura) - 58` */
+        Scanner scan = new Scanner(System.in);
+        double altura,pIdeal;
+
+        System.out.println("** Calculo peso ideal **");
+        System.out.println("Digite a sua altura: ");
+        altura = scan.nextDouble();
+
+        pIdeal = (72.7 * altura) - 58;
+
+        System.out.println("Seu peso ideal eh: " + pIdeal);
+    }
+    
+}
